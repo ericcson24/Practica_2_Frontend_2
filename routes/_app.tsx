4be28d2cfad5a4f8740e8966f3637f1a3dd7bad9
ericcson24 <150656730+ericcson24@users.noreply.github.com>
2025-03-19 13:40:37 +0100

    Update _app.tsx

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -7,7 +7,7 @@ export default function App(props: PageProps) {
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Rick & Fresh</title>
+        <title>Practica2</title>
         <link rel="stylesheet" href="/styles.css" />
       </head>
       <body>
